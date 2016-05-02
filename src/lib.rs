f8b3f48faf0a0678d3f49bd886103dbea2475d69
Daniel Reiter Horn <danielrh@dropbox.com>
2016-05-01 22:17:14 -0700

    some lifetime errors: need to refactor RefCell::input

--- src/lib.rs
+++ src/lib.rs
@@ -3,35 +3,76 @@ extern crate alloc_no_stdlib as alloc;
 extern crate brotli_no_stdlib;
 extern crate core;
 use std::io;
+use core::cell;
 mod heap_alloc;
 use heap_alloc::{HeapAllocator, Mem};
-use brotli_no_stdlib::{HuffmanCode, BrotliState};
+use brotli_no_stdlib::{HuffmanCode, BrotliState, BrotliDecompressStream, BrotliResult};
 use alloc::{Allocator, SliceWrapperMut, SliceWrapper, StackAllocator, AllocatedStackMemory};
 
 
 pub struct Decompressor<'a, R: io::Read> {
+    input_buffer : [u8; 65536],
+    scratch : [u8; 8],
+    total_out : usize,
+    input_offset : usize,
+    input_len : usize,
+    input_eof : bool,
     input: R,
-    alloc_u8 : HeapAllocator<u8>,
-    alloc_u32 : HeapAllocator<u32>,
-    alloc_hc : HeapAllocator<HuffmanCode>,
-    state : Option<BrotliState<'a, HeapAllocator<u8>, HeapAllocator<u32>, HeapAllocator<HuffmanCode> > >,
+    state : BrotliState<'a, HeapAllocator<u8>, HeapAllocator<u32>, HeapAllocator<HuffmanCode> >,
 }
 
 impl<'a, R: io::Read> Decompressor<'a, R> {
     pub fn new(r: R) -> Decompressor<'a, R> {
-        let ret = Decompressor{
+        return Decompressor{
+            input_buffer : [0; 65536],
+            scratch : [0; 8],
+            total_out : 0,
+            input_offset : 0,
+            input_len : 0,
+            input_eof : false,
             input: r,
-            alloc_u8 : HeapAllocator::<u8>{default_value : 0u8},
-            alloc_u32 : HeapAllocator::<u32>{default_value : 0u32},
-            alloc_hc : HeapAllocator::<HuffmanCode>{default_value : HuffmanCode::default()},
-            state : None,
+            state : BrotliState::new(HeapAllocator::<u8>{default_value : 0u8},
+                                     HeapAllocator::<u32>{default_value : 0u32},
+                                     HeapAllocator::<HuffmanCode>{
+                                         default_value : HuffmanCode::default()}),
         };
-        ret.state = Some(BrotliState::new(ret.alloc_u8, ret.alloc_u32, ret.alloc_hc));
-        return ret
+    }
+    pub fn copy_to_front(&mut self) {
+        if self.input_offset == self.input_buffer.len() {
+            self.input_offset = 0;// FIXME
+            self.input_len = 0;
+        }
     }
 }
 impl<'a, R: io::Read> io::Read for Decompressor<'a, R> {
 	fn read(&mut self, mut buf: &mut [u8]) -> io::Result<usize> {
-            Err(io::Error::new(io::ErrorKind::InvalidData, "Unimplemented"))
+            let mut output_offset : usize = 0;
+            let mut avail_out = buf.len() - output_offset;
+            let mut avail_in = self.input_len - self.input_offset;
+            while avail_out == buf.len() && !self.input_eof {
+                    match self.input.read(&mut self.input_buffer[self.input_len..]) {
+                        Err(e) => self.input_eof = true,
+                        Ok(size) => self.input_len += size,
+                    }
+                    let scratch_ref_cell = cell::RefCell::<&'a mut[u8]>::new(&mut self.scratch[..]);
+                    let input_ref_cell = cell::RefCell::<&'a mut[u8]>::new(&mut self.input_buffer[..]);
+                    match BrotliDecompressStream(&mut avail_in,
+                                                  &mut self.input_offset,
+                                                  &input_ref_cell,
+                                                  &mut avail_out,
+                                                  &mut output_offset,
+                                                  buf,
+                                                  &mut self.total_out,
+                                                  &mut self.state,
+                                                  &scratch_ref_cell) {
+                        BrotliResult::NeedsMoreInput => self.copy_to_front(),
+                        BrotliResult::NeedsMoreOutput => {},
+                        BrotliResult::ResultSuccess => {},
+                        BrotliResult::ResultFailure =>
+                            return Err(io::Error::new(io::ErrorKind::InvalidData,
+                                                      "Invalid Data")),
+                  }
+            }
+            return Ok(buf.len() - output_offset);
         }
 }
