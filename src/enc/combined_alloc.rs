a14e0f079242f4b64789496ae0dd5971c88d0312
Daniel Reiter Horn <danielrh@users.sourceforge.net>
2018-09-08 00:17:19 -0700

    make the combined_alloc more friendly for older versions of rust

--- src/enc/combined_alloc.rs
+++ src/enc/combined_alloc.rs
@@ -1,17 +1,20 @@
 pub use alloc::Allocator;
-use super::Command;
+use super::command::Command;
 use super::util::floatX;
 use super::v8;
 use super::s16;
 use super::PDF;
-use super::StaticCommand;
-use super::HistogramLiteral;
-use super::HistogramCommand;
-use super::HistogramDistance;
-use super::HistogramPair;
-use super::ContextType;
-use super::HuffmanTree;
-use super::ZopfliNode;
+use super::interface::StaticCommand;
+use super::histogram::{
+  HistogramLiteral,
+  HistogramCommand,
+  HistogramDistance,
+  ContextType,
+};
+use super::cluster::HistogramPair;
+
+use super::entropy_encode::HuffmanTree;
+use super::hash_to_binary_tree::ZopfliNode;
 
 /*
 struct CombiningAllocator<T1, T2, AllocT1:Allocator<T1>, AllocT2:Allocator<T2>>(AllocT1, AllocT2);
@@ -38,19 +41,19 @@ pub struct CombiningAllocator<AllocU8:Allocator<u8>,
                           AllocI32:Allocator<i32>,
                           AllocU32:Allocator<u32>,
                           AllocU64:Allocator<u64>,
-                          AllocCommand:Allocator<super::Command>,
-                          AllocFloatX:Allocator<super::util::floatX>,
-                          AllocV8:Allocator<super::v8>,
-                          AllocS16:Allocator<super::s16>,
-                          AllocPDF:Allocator<super::PDF>,
-                          AllocStaticCommand:Allocator<super::StaticCommand>,
-                          AllocHistogramLiteral:Allocator<super::HistogramLiteral>,
-                          AllocHistogramCommand:Allocator<super::HistogramCommand>,
-                          AllocHistogramDistance:Allocator<super::HistogramDistance>,
-                          AllocHistogramPair:Allocator<super::HistogramPair>,
-                          AllocContextType:Allocator<super::ContextType>,
-                          AllocHuffmanTree:Allocator<super::HuffmanTree>,
-                          AllocZopfliNode:Allocator<super::ZopfliNode>,
+                          AllocCommand:Allocator<Command>,
+                          AllocFloatX:Allocator<floatX>,
+                          AllocV8:Allocator<v8>,
+                          AllocS16:Allocator<s16>,
+                          AllocPDF:Allocator<PDF>,
+                          AllocStaticCommand:Allocator<StaticCommand>,
+                          AllocHistogramLiteral:Allocator<HistogramLiteral>,
+                          AllocHistogramCommand:Allocator<HistogramCommand>,
+                          AllocHistogramDistance:Allocator<HistogramDistance>,
+                          AllocHistogramPair:Allocator<HistogramPair>,
+                          AllocContextType:Allocator<ContextType>,
+                          AllocHuffmanTree:Allocator<HuffmanTree>,
+                          AllocZopfliNode:Allocator<ZopfliNode>,
                           >{
   alloc_u8: AllocU8,
   alloc_u16: AllocU16,
@@ -77,19 +80,19 @@ impl<AllocU8:Allocator<u8>,
      AllocI32:Allocator<i32>,
      AllocU32:Allocator<u32>,
      AllocU64:Allocator<u64>,
-     AllocCommand:Allocator<super::Command>,
-     AllocFloatX:Allocator<::enc::util::floatX>,
-     AllocV8:Allocator<super::v8>,
-     AllocS16:Allocator<super::s16>,
-     AllocPDF:Allocator<super::PDF>,
-     AllocStaticCommand:Allocator<super::StaticCommand>,
-     AllocHistogramLiteral:Allocator<super::HistogramLiteral>,
-     AllocHistogramCommand:Allocator<super::HistogramCommand>,
-     AllocHistogramDistance:Allocator<super::HistogramDistance>,
-     AllocHistogramPair:Allocator<super::HistogramPair>,
-     AllocContextType:Allocator<super::ContextType>,
-     AllocHuffmanTree:Allocator<super::HuffmanTree>,
-     AllocZopfliNode:Allocator<super::ZopfliNode>,
+     AllocCommand:Allocator<Command>,
+     AllocFloatX:Allocator<floatX>,
+     AllocV8:Allocator<v8>,
+     AllocS16:Allocator<s16>,
+     AllocPDF:Allocator<PDF>,
+     AllocStaticCommand:Allocator<StaticCommand>,
+     AllocHistogramLiteral:Allocator<HistogramLiteral>,
+     AllocHistogramCommand:Allocator<HistogramCommand>,
+     AllocHistogramDistance:Allocator<HistogramDistance>,
+     AllocHistogramPair:Allocator<HistogramPair>,
+     AllocContextType:Allocator<ContextType>,
+     AllocHuffmanTree:Allocator<HuffmanTree>,
+     AllocZopfliNode:Allocator<ZopfliNode>,
      > CombiningAllocator<AllocU8,
                           AllocU16,
                           AllocI32,
@@ -156,19 +159,19 @@ impl<AllocU8:Allocator<u8>,
      AllocI32:Allocator<i32>,
      AllocU32:Allocator<u32>,
      AllocU64:Allocator<u64>,
-     AllocCommand:Allocator<super::Command>,
-     AllocFloatX:Allocator<super::util::floatX>,
-     AllocV8:Allocator<super::v8>,
-     AllocS16:Allocator<super::s16>,
-     AllocPDF:Allocator<super::PDF>,
-     AllocStaticCommand:Allocator<super::StaticCommand>,
-     AllocHistogramLiteral:Allocator<super::HistogramLiteral>,
-     AllocHistogramCommand:Allocator<super::HistogramCommand>,
-     AllocHistogramDistance:Allocator<super::HistogramDistance>,
-     AllocHistogramPair:Allocator<super::HistogramPair>,
-     AllocContextType:Allocator<super::ContextType>,
-     AllocHuffmanTree:Allocator<super::HuffmanTree>,
-     AllocZopfliNode:Allocator<super::ZopfliNode>,
+     AllocCommand:Allocator<Command>,
+     AllocFloatX:Allocator<floatX>,
+     AllocV8:Allocator<v8>,
+     AllocS16:Allocator<s16>,
+     AllocPDF:Allocator<PDF>,
+     AllocStaticCommand:Allocator<StaticCommand>,
+     AllocHistogramLiteral:Allocator<HistogramLiteral>,
+     AllocHistogramCommand:Allocator<HistogramCommand>,
+     AllocHistogramDistance:Allocator<HistogramDistance>,
+     AllocHistogramPair:Allocator<HistogramPair>,
+     AllocContextType:Allocator<ContextType>,
+     AllocHuffmanTree:Allocator<HuffmanTree>,
+     AllocZopfliNode:Allocator<ZopfliNode>,
      > Allocator<u8> for CombiningAllocator<AllocU8,
                                             AllocU16,
                                             AllocI32,
@@ -199,27 +202,28 @@ impl<AllocU8:Allocator<u8>,
 
 
 macro_rules! implement_allocator {
-  ($bound_name: tt,
-   $type_name: tt,
-   $local_name: tt) => {
+  ($bound_name: ty,
+   $type_name: ty,
+   $sub_type_name: ty,
+   $local_name: ident) => {
     impl<AllocU8:Allocator<u8>,
          AllocU16:Allocator<u16>,
          AllocI32:Allocator<i32>,
          AllocU32:Allocator<u32>,
          AllocU64:Allocator<u64>,
-         AllocCommand:Allocator<super::Command>,
-         AllocFloatX:Allocator<super::util::floatX>,
-         AllocV8:Allocator<super::v8>,
-         AllocS16:Allocator<super::s16>,
-         AllocPDF:Allocator<super::PDF>,
-         AllocStaticCommand:Allocator<super::StaticCommand>,
-         AllocHistogramLiteral:Allocator<super::HistogramLiteral>,
-         AllocHistogramCommand:Allocator<super::HistogramCommand>,
-         AllocHistogramDistance:Allocator<super::HistogramDistance>,
-         AllocHistogramPair:Allocator<super::HistogramPair>,
-         AllocContextType:Allocator<super::ContextType>,
-         AllocHuffmanTree:Allocator<super::HuffmanTree>,
-         AllocZopfliNode:Allocator<super::ZopfliNode>,
+         AllocCommand:Allocator<Command>,
+         AllocFloatX:Allocator<floatX>,
+         AllocV8:Allocator<v8>,
+         AllocS16:Allocator<s16>,
+         AllocPDF:Allocator<PDF>,
+         AllocStaticCommand:Allocator<StaticCommand>,
+         AllocHistogramLiteral:Allocator<HistogramLiteral>,
+         AllocHistogramCommand:Allocator<HistogramCommand>,
+         AllocHistogramDistance:Allocator<HistogramDistance>,
+         AllocHistogramPair:Allocator<HistogramPair>,
+         AllocContextType:Allocator<ContextType>,
+         AllocHuffmanTree:Allocator<HuffmanTree>,
+         AllocZopfliNode:Allocator<ZopfliNode>,
          > Allocator<$type_name> for CombiningAllocator<AllocU8,
                                                  AllocU16,
                                                  AllocI32,
@@ -239,7 +243,7 @@ macro_rules! implement_allocator {
                                                  AllocHuffmanTree,
                                                  AllocZopfliNode,
                                                  > {
-      type AllocatedMemory = $bound_name::AllocatedMemory;
+      type AllocatedMemory = $sub_type_name;
       fn alloc_cell(&mut self, size: usize) -> <Self as Allocator<$type_name>>::AllocatedMemory {
         self.$local_name.alloc_cell(size)
       }
@@ -250,20 +254,22 @@ macro_rules! implement_allocator {
 };
 }
 
-implement_allocator!(AllocU16, u16, alloc_u16);
-implement_allocator!(AllocI32, i32, alloc_i32);
-implement_allocator!(AllocU32, u32, alloc_u32);
-implement_allocator!(AllocU64, u64, alloc_u64);
-implement_allocator!(AllocCommand, Command, alloc_c);
-implement_allocator!(AllocFloatX, floatX, alloc_f);
-implement_allocator!(AllocV8, v8, alloc_f32x8);
-implement_allocator!(AllocS16, s16, alloc_i16x16);
-implement_allocator!(AllocPDF, PDF, alloc_pdf);
-implement_allocator!(AllocStaticCommand, StaticCommand, alloc_sc);
-implement_allocator!(AllocHistogramLiteral, HistogramLiteral, alloc_hl);
-implement_allocator!(AllocHistogramCommand, HistogramCommand, alloc_hc);
-implement_allocator!(AllocHistogramDistance, HistogramDistance, alloc_hd);
-implement_allocator!(AllocHistogramPair, HistogramPair, alloc_hp);
-implement_allocator!(AllocContextType, ContextType, alloc_ct);
-implement_allocator!(AllocHuffmanTree, HuffmanTree, alloc_ht);
-implement_allocator!(AllocZopfliNode, ZopfliNode, alloc_zn);
+implement_allocator!(AllocU16, u16, AllocU16::AllocatedMemory, alloc_u16);
+
+implement_allocator!(AllocI32, i32, AllocI32::AllocatedMemory, alloc_i32);
+implement_allocator!(AllocU32, u32, AllocU32::AllocatedMemory, alloc_u32);
+implement_allocator!(AllocU64, u64, AllocU64::AllocatedMemory, alloc_u64);
+implement_allocator!(AllocCommand, Command, AllocCommand::AllocatedMemory, alloc_c);
+implement_allocator!(AllocFloatX, floatX, AllocFloatX::AllocatedMemory, alloc_f);
+implement_allocator!(AllocV8, v8, AllocV8::AllocatedMemory, alloc_f32x8);
+implement_allocator!(AllocS16, s16, AllocS16::AllocatedMemory, alloc_i16x16);
+implement_allocator!(AllocPDF, PDF, AllocPDF::AllocatedMemory, alloc_pdf);
+implement_allocator!(AllocStaticCommand, StaticCommand, AllocStaticCommand::AllocatedMemory, alloc_sc);
+implement_allocator!(AllocHistogramLiteral, HistogramLiteral, AllocHistogramLiteral::AllocatedMemory, alloc_hl);
+implement_allocator!(AllocHistogramCommand, HistogramCommand, AllocHistogramCommand::AllocatedMemory, alloc_hc);
+implement_allocator!(AllocHistogramDistance, HistogramDistance, AllocHistogramDistance::AllocatedMemory, alloc_hd);
+implement_allocator!(AllocHistogramPair, HistogramPair, AllocHistogramPair::AllocatedMemory, alloc_hp);
+implement_allocator!(AllocContextType, ContextType, AllocContextType::AllocatedMemory, alloc_ct);
+implement_allocator!(AllocHuffmanTree, HuffmanTree, AllocHuffmanTree::AllocatedMemory, alloc_ht);
+implement_allocator!(AllocZopfliNode, ZopfliNode, AllocZopfliNode::AllocatedMemory, alloc_zn);
+
