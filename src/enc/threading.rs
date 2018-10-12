ad3607095a00d625ecb044f37c5a5574c3004103
Daniel Reiter Horn <danielrh@users.sourceforge.net>
2018-10-12 02:08:28 -0700

    a bit of trait magic to get CompressMultiSlice to call CompressMulti and allocate a temp buffer before calling it and freeing it after

--- src/enc/threading.rs
+++ src/enc/threading.rs
@@ -1,4 +1,5 @@
-use alloc::{Allocator, SliceWrapper};
+use alloc::{Allocator, SliceWrapper, SliceWrapperMut};
+use core::marker::PhantomData;
 use super::BrotliAlloc;
 use super::backward_references::BrotliEncoderParams;
 pub trait Joinable<T:Send+'static>:Sized {
@@ -11,27 +12,28 @@ pub struct CompressionThreadResult<Alloc:BrotliAlloc+Send+'static> where <Alloc 
   compressed_size: usize,
   alloc: Alloc,
 }
-enum InternalSendAlloc<Alloc:BrotliAlloc+Send+'static, Join: Joinable<CompressionThreadResult<Alloc>>>
+enum InternalSendAlloc<T:Send+'static, Alloc:BrotliAlloc+Send+'static, Join: Joinable<T>>
   where <Alloc as Allocator<u8>>::AllocatedMemory: Send {
   A(Alloc),
   Join(Join),
-  Spawning,
+  Spawning(PhantomData<T>),
 }
-pub struct SendAlloc<Alloc:BrotliAlloc +Send+'static,
-                     Join:Joinable<CompressionThreadResult<Alloc>>>(InternalSendAlloc<Alloc, Join>)
+pub struct SendAlloc<T:Send+'static,
+                     Alloc:BrotliAlloc +Send+'static,
+                     Join:Joinable<T>>(InternalSendAlloc<T, Alloc, Join>)
   where <Alloc as Allocator<u8>>::AllocatedMemory: Send;
 
-impl<Alloc:BrotliAlloc+Send+'static,Join:Joinable<CompressionThreadResult<Alloc>>> SendAlloc<Alloc, Join>
+impl<T:Send+'static, Alloc:BrotliAlloc+Send+'static,Join:Joinable<T>> SendAlloc<T, Alloc, Join>
   where <Alloc as Allocator<u8>>::AllocatedMemory: Send {
   pub fn new(alloc: Alloc) -> Self {
-    SendAlloc::<Alloc, Join>(InternalSendAlloc::A(alloc))
+    SendAlloc::<T, Alloc, Join>(InternalSendAlloc::A(alloc))
   }
   pub fn unwrap_or(self, other: Alloc) -> Alloc {
     match self.0 {
       InternalSendAlloc::A(alloc) => {
         alloc
       },
-      InternalSendAlloc::Spawning | InternalSendAlloc::Join(_) => {
+      InternalSendAlloc::Spawning(_) | InternalSendAlloc::Join(_) => {
         other
       },
     }
@@ -41,7 +43,7 @@ impl<Alloc:BrotliAlloc+Send+'static,Join:Joinable<CompressionThreadResult<Alloc>
       InternalSendAlloc::A(alloc) => {
         alloc
       },
-      InternalSendAlloc::Join(_) | InternalSendAlloc::Spawning => panic!("Item permanently borrowed/leaked"),
+      InternalSendAlloc::Join(_) | InternalSendAlloc::Spawning(_) => panic!("Item permanently borrowed/leaked"),
     }
   }
 }
@@ -74,36 +76,80 @@ impl<T> Owned<T> {
 }
 
 pub trait OwnedRetriever<SliceW: SliceWrapper<u8>+Send+'static> {
+  fn view(&self) -> &[u8];
   fn unwrap(self) -> SliceW;
 }
 
-pub trait BatchSpawnable<Alloc:BrotliAlloc+Send+'static, SliceW:SliceWrapper<u8>+Send+'static>
+pub trait BatchSpawnable<T:Send+'static, Alloc:BrotliAlloc+Send+'static, SliceW:SliceWrapper<u8>+Send+'static>
   where <Alloc as Allocator<u8>>::AllocatedMemory:Send+'static
 {
-  type JoinHandle: Joinable<CompressionThreadResult<Alloc>>;
+  type JoinHandle: Joinable<T>;
   type FinalJoinHandle: OwnedRetriever<SliceW>;
   // this function takes in a SendAlloc per thread and converts them all into JoinHandle
   // the input is borrowed until the joins complete
   // owned is set to borrowed
   // the final join handle is a r/w lock which will return the SliceW to the owner
   // the FinalJoinHandle is only to be called when each individual JoinHandle has been examined
-  fn batch_spawn<F: FnOnce() -> CompressionThreadResult<Alloc> + Send + 'static>(
+  fn batch_spawn<F: FnOnce(usize, &SliceW, Alloc) -> T>(
+    &mut self,
     input: &mut Owned<SliceW>,
-    alloc_per_thread:&mut [SendAlloc<Alloc, Self::JoinHandle>],
+    alloc_per_thread:&mut [SendAlloc<T, Alloc, Self::JoinHandle>],
     f: F,
   ) -> Self::FinalJoinHandle;
 }
 
 
 
-pub fn CompressMulti<Alloc:BrotliAlloc+Send+'static, SliceW: SliceWrapper<u8>+Send+'static, Spawner:BatchSpawnable<Alloc, SliceW>> (
+pub fn CompressMultiSlice<Alloc:BrotliAlloc+Send+'static,
+                          Spawner:BatchSpawnable<CompressionThreadResult<Alloc>,
+                                                 Alloc,
+                                                 <Alloc as Allocator<u8>>::AllocatedMemory>> (
   params:&BrotliEncoderParams,
-  input: &mut Owned<SliceW>,
+  input_slice: &[u8],
   output: &mut [u8],
-  alloc_per_thread:&mut [SendAlloc<Alloc, Spawner::JoinHandle>],
-  thread_spawner: Spawner,
+  alloc_per_thread:&mut [SendAlloc<CompressionThreadResult<Alloc>, Alloc, Spawner::JoinHandle>],
+  mut thread_spawner: Spawner,
 ) -> Result<usize, ()> where <Alloc as Allocator<u8>>::AllocatedMemory: Send {
-  
+  let input = if let InternalSendAlloc::A(ref mut alloc) = alloc_per_thread[0].0 {
+    let mut input = <Alloc as Allocator<u8>>::alloc_cell(alloc, input_slice.len());
+    input.slice_mut().clone_from_slice(input_slice);
+    input
+  } else {
+    <Alloc as Allocator<u8>>::AllocatedMemory::default()
+  };
+  let mut owned_input = Owned::new(input);
+  let ret = CompressMulti(params, &mut owned_input, output, alloc_per_thread,thread_spawner);
+  if let InternalSendAlloc::A(ref mut alloc) = alloc_per_thread[0].0 {
+    <Alloc as Allocator<u8>>::free_cell(alloc, owned_input.unwrap());
+  }
+  ret
+}
+
+
+pub fn CompressMulti<Alloc:BrotliAlloc+Send+'static,
+                     SliceW: SliceWrapper<u8>+Send+'static,
+                     Spawner:BatchSpawnable<CompressionThreadResult<Alloc>,
+                                            Alloc,
+                                            SliceW>> (
+  params:&BrotliEncoderParams,
+  owned_input: &mut Owned<SliceW>,
+  output: &mut [u8],
+  alloc_per_thread:&mut [SendAlloc<CompressionThreadResult<Alloc>, Alloc, Spawner::JoinHandle>],
+  mut thread_spawner: Spawner,
+) -> Result<usize, ()> where <Alloc as Allocator<u8>>::AllocatedMemory: Send {
+  let (mut alloc, alloc_rest) = alloc_per_thread.split_at_mut(1);
+  let mut retrieve_owned_input = thread_spawner.batch_spawn(owned_input, alloc_rest, |_index,_input,mut alloc|{
+    let mem = <Alloc as Allocator<u8>>::alloc_cell(&mut alloc, 0);
+    CompressionThreadResult::<Alloc>{
+      compressed:mem,
+      compressed_size:0,
+      alloc:alloc,
+    }
+  });
+  {
+    let input = retrieve_owned_input.view();
+    
+  }
   Err(())
 }
 
