08fc11aa3b412679b5062becd2174a240eae72ce
Daniel Reiter Horn <danielrh@dropbox.com>
2018-10-23 20:36:45 -0700

    some fixes for older compilers and checking the shasum

--- src/bin/validate.rs
+++ src/bin/validate.rs
@@ -1,8 +1,8 @@
 use super::BrotliEncoderParams;
-use std::io::{self, Error, ErrorKind, Read, Write, Seek, SeekFrom};
+use std::io::{self, Error, ErrorKind, Read, Write};
 use super::{Rebox, HeapAllocator, IoWriterWrapper};
-use brotli::{DecompressorWriterCustomIo, HuffmanCode, CustomWrite};
-use alloc_no_stdlib::{Allocator, SliceWrapper,SliceWrapperMut};
+use brotli::{DecompressorWriterCustomIo, CustomWrite};
+use alloc_no_stdlib::{Allocator, SliceWrapper};
 
 
 
@@ -20,15 +20,17 @@ impl <OutputA:Write, OutputB:Write> Write for Tee<OutputA, OutputB> {
     fn flush(&mut self) -> Result<(), io::Error> {
         match self.0.flush() {
             Err(err) => return Err(err),
-            Ok(_) => loop {
-                match self.1.flush() {
-                    Err(e) => match e.kind() {
-                        ErrorKind::Interrupted => continue,
-                        _ => return Err(e),
+            Ok(_) => {
+                loop {
+                    match self.1.flush() {
+                        Err(e) => match e.kind() {
+                            ErrorKind::Interrupted => continue,
+                            _ => return Err(e),
+                        }
+                        Ok(e) => return Ok(e),
                     }
-                    Ok(e) => return Ok(e),
                 }
-            }
+            },
         }
     }
 }
@@ -55,10 +57,18 @@ fn make_sha_writer() -> io::Sink {
     io::sink()
 }
 
+fn make_sha_reader<InputType:Read>(r:&mut InputType) -> &mut InputType {
+    r
+}
+
+fn sha_ok<InputType:Read>(_writer: io::Sink, _reader: &mut InputType) -> bool {
+    false
+}
 
 struct ShaReader<InputType:Read> {
     reader:InputType,
 }
+
 
 
 pub fn compress_validate<InputType:Read, OutputType: Write>(r: &mut InputType,
@@ -76,20 +86,29 @@ pub fn compress_validate<InputType:Read, OutputType: Write>(r: &mut InputType,
 
     dict.extend_from_slice(custom_dictionary.slice());
     let mut sha_writer = make_sha_writer();
-    let mut validate_writer = DecompressAndValidate(DecompressorWriterCustomIo::new_with_custom_dictionary(
-        IoWriterWrapper(&mut sha_writer),
-        buffer,
-        m8,
-        HeapAllocator::default(),
-        HeapAllocator::default(),
-        custom_dictionary,
-        Error::new(ErrorKind::InvalidData,
-                   "Invalid Data")));
-    let mut overarching_writer = Tee(validate_writer, w);
-    let ret = super::compress(r, &mut overarching_writer, buffer_size, params, &dict[..], num_threads);
+    let mut sha_reader = make_sha_reader(r);
+    let ret;
+    {
+        let validate_writer = DecompressAndValidate(DecompressorWriterCustomIo::new_with_custom_dictionary(
+            IoWriterWrapper(&mut sha_writer),
+            buffer,
+            m8,
+            HeapAllocator::default(),
+            HeapAllocator::default(),
+            custom_dictionary,
+            Error::new(ErrorKind::InvalidData,
+                       "Invalid Data")));
+        let mut overarching_writer = Tee(validate_writer, w);
+        ret = super::compress(sha_reader, &mut overarching_writer, buffer_size, params, &dict[..], num_threads);
+    }
     match ret {
-        //FIXME: check sig here
-        Ok(ret) => Ok(()),
+        Ok(_ret) => {
+            if sha_ok(sha_writer, sha_reader) {
+                return Ok(());
+            } else {
+                return Err(Error::new(ErrorKind::InvalidData, "Validation failed"));
+            }
+        },
         Err(e) => Err(e),
     }
 }
